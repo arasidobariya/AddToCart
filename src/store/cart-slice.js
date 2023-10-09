0c10de5671790909a979aad34dee59f940456b74
Arasi Dobariya <arasidobariya7@gmail.com>
2023-10-08 23:06:07 -0400

    created cartslice component

---
+++ src/store/cart-slice.js
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+createSlice({
+  name: "cart",
+  initialState: {
+    items: [],
+    totalQuantity: 0,
+  },
+  reducers: {
+    addItemToCart(state, action) {
+      const newItem = action.payload;
+      const existingItem = state.items.find((item) => item.id === newItem.id);
+      if (!existingItem) {
+        state.items.push();
+      }
+    },
+    removeItemToCart() {},
+  },
+});
